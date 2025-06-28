fe6c461d84d0cfe69a88497888cdfbc2cc909f3d
Saurav-coder-pixel <sauravkumar7678g@gmail.com>
2025-06-28 13:02:47 +0530

    Update TwoSum.java

--- TwoSum.java
+++ TwoSum.java
@@ -1,16 +1,23 @@
-class 26 {
-    public int removeDuplicates(int[] nums) {
-        if(nums.length==0){
-            return 0;
-        }
-        int k= 1;
-        for(int i=0; i<nums.length; i++){
-            if(nums[i]!= nums[k-1]){
-                nums[k]=nums[i];
-                k++;
+public class TwoSum {
+
+    public static int[] twoSum(int[] nums, int target) {
+       int n= nums.length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[] { i, j };
+                }
             }
         }
-        return k;
+        
+        return null;
+    }
+    public static void main(String[] args){
+        int nums[]= {2,7,11,15};
+        int target= 9;
+        int[] result = twoSum(nums, target);
+        System.out.println(result);
     }
 }
 
