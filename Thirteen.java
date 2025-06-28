991a63e5b2d65c3d598835d0921e071520243d8d
Saurav-coder-pixel <sauravkumar7678g@gmail.com>
2025-06-28 13:01:38 +0530

    Update Thirteen.java

--- Thirteen.java
+++ Thirteen.java
@@ -1,10 +1,33 @@
-class Solution {
-    public double myPow(double x, int n) {
-        if(n == 0)
-            return 1;
-        if(n<0){
-            return 1/x * myPow(1/x, -(n + 1));
+class Thirteen {
+    public static int getValue(char ch) {
+        if (ch == 'I') return 1;
+        if (ch == 'V') return 5;
+        if (ch == 'X') return 10;
+        if (ch == 'L') return 50;
+        if (ch == 'C') return 100;
+        if (ch == 'D') return 500;
+        if (ch == 'M') return 1000;
+        return 0;
+    }
+    
+    public static int romanToInt(String s) {
+        int total=0;
+        int preValue =0;
+        for(int i = s.length() - 1; i >= 0; i--){
+            int current =getValue(s.charAt(i));
+            
+            
+            if(current>= preValue){
+             total= total + current;
+            }else{
+                total= total - current; 
+            }
+             preValue = current;
         }
-        return (n%2 == 0) ? myPow(x*x, n/2) : x*myPow(x*x, n/2);
+         return total;
+    }
+    public static void main(String[] args){
+        String s= "III";
+        System.out.println(romanToInt(s));
     }
 }
