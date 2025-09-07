7fd4dd7eac80681ea241b8d2b48aa0615aa2e377
Saurav Kumar <sauravkumar7678g@gmail.com>
2025-09-07 20:27:31 +0530

    Update 17.java

--- 17.java
+++ 17.java
@@ -25,3 +25,56 @@ class Solution {
         }
     }
 }
+
+
+
+//Better time complixity
+class Solution {
+
+    private List<String> combinations = new ArrayList<>();
+    private Map<Character, String> letters = Map.of(
+        '2',
+        "abc",
+        '3',
+        "def",
+        '4',
+        "ghi",
+        '5',
+        "jkl",
+        '6',
+        "mno",
+        '7',
+        "pqrs",
+        '8',
+        "tuv",
+        '9',
+        "wxyz"
+    );
+    private String digits;
+
+    public List<String> letterCombinations(String digits) {
+        if (digits.length() == 0) {
+            return combinations;
+        }
+
+        this.digits = digits;
+        backtrack(0, new StringBuilder());
+        return combinations;
+    }
+
+    private void backtrack(int index, StringBuilder path) {
+        if (path.length() == digits.length()) {
+            combinations.add(path.toString());
+            return;
+        }
+
+        String word = letters.get(digits.charAt(index));
+
+        for (char c : word.toCharArray()) {
+            path.append(c);
+            backtrack(index + 1, path);
+            path.deleteCharAt(path.length() - 1);
+        }
+
+    }
+}
