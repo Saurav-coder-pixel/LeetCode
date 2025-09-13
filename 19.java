53d2a7ad5b3c6ebcb16b3f682f1a96a221159cf7
Saurav Kumar <sauravkumar7678g@gmail.com>
2025-09-13 13:17:59 +0530

    Update 19.java

--- 19.java
+++ 19.java
@@ -24,3 +24,30 @@ class Solution {
         return head;
         }
     }
+
+// Better time compxity
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if(head.next== null){
+            return null;
+        }
+
+        ListNode slow= head;
+        ListNode fast= head;
+        for(int i=1; i<=n; i++){
+            fast= fast.next;
+        }
+          if(fast== null){
+            head= head.next;
+            return head;
+        }
+
+        while(fast.next!= null){
+            slow= slow.next;
+            fast= fast.next;
+        }
+
+        slow.next= slow.next.next;
+        return head;
+        }
+    }
