e0dc9d70e1ba1f304e2ad5ad3927185861bd2525
Saurav Kumar <sauravkumar7678g@gmail.com>
2025-10-07 03:38:38 +0530

    Update 141.java

--- 141.java
+++ 141.java
@@ -27,3 +27,31 @@ public class Solution {
         return false;
     }
 }
+
+//Better time complixity
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow= head;
+        ListNode fast= head;
+        if(head== null){
+            return false;
+        }
+        if(head.next== null){
+            return false;
+        }
+        while(fast!= null){
+            if(slow== null){
+                return false;
+            }
+            slow= slow.next;
+            if(fast.next== null){
+                return false;
+            }
+            fast= fast.next.next;
+            if(fast== slow){
+                return true;
+            }
+        }
+        return false;
+    }
+}
