2b9a8081d05f5771ff2da1d41f10ed1024fb6202
Saurav Kumar <sauravkumar7678g@gmail.com>
2025-10-11 07:42:35 +0530

    Update 234.java

--- 234.java
+++ 234.java
@@ -62,3 +62,46 @@ class Solution {
         return prev;
     }
 }
+
+
+// Better method
+
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+
+        ListNode slow= head;
+        ListNode fast= head;
+        while(fast.next!= null && fast.next.next!= null){
+            slow= slow.next;
+            fast= fast.next.next;
+        }
+        ListNode temp= reverseList(slow.next);
+        slow.next= temp;
+        ListNode p1= head;
+        ListNode p2= slow.next;
+        while(p2!= null){
+            if(p1.val!= p2.val) return false;
+            p1= p1.next;
+            p2= p2.next;
+        }
+        return true;
+    }
+    
+    private ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+        ListNode Next= null;
+        
+        while (current != null) {
+            Next = current.next;
+            current.next = prev;
+            prev = current;
+            current = Next;
+        }
+        
+        return prev;
+    }
+}
