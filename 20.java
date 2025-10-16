717709f025e947658cd2ed49f0f666bc2dff3c3a
Saurav Kumar <sauravkumar7678g@gmail.com>
2025-10-16 20:25:10 +0530

    Update 20.java

--- 20.java
+++ 20.java
@@ -24,26 +24,25 @@ class Solution {
 // best time complixity
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> st = new Stack<>();
+        Stack<Character> stack = new Stack<>();
+        int n= s.length();
 
-        for(int i = 0; i<s.length();i++){
-            char ch = s.charAt(i);
-            if(ch=='{'||ch=='('||ch=='['){
-                st.push(ch);
-            }
+        for (int i=0; i<n; i++) {
+            char ch= s.charAt(i);
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            } else {
+                if (stack.isEmpty()) return false; 
+                char top = stack.pop();
 
-            else {
-                if (st.isEmpty()) return false;
-                char top = st.peek();
-            if((ch==']'&&top=='[')||(ch=='}'&&top=='{')||(ch==')'&&top=='(')){
-                st.pop();
-            }else{
-                return false;
-            }
-
+                if ((ch == ')' && top != '(') ||
+                    (ch == '}' && top != '{') ||
+                    (ch == ']' && top != '[')) {
+                    return false; 
+                }
             }
         }
 
-        return st.isEmpty();
+        return stack.isEmpty();
     }
 }
