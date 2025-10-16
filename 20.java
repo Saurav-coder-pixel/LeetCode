915e245a921d569533c122ef323cb08faa3828de
Saurav Kumar <sauravkumar7678g@gmail.com>
2025-10-16 22:03:09 +0530

    Update 20.java

--- 20.java
+++ 20.java
@@ -46,3 +46,31 @@ class Solution {
         return stack.isEmpty();
     }
 }
+
+
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        int n= s.length();
+
+        for (int i=0; i<n; i++) {
+            char ch= s.charAt(i);
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            } else {
+                if (stack.isEmpty()) return false; 
+
+                if ((ch == ')' && stack.peek()== '(') ||
+                    (ch == '}' && stack.peek()== '{') ||
+                    (ch == ']' && stack.peek()== '[')) {
+                    stack.pop(); 
+                }else{
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
