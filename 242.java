2bc942d933b651cc9011de3e7220019f878a4196
Saurav Kumar <sauravkumar7678g@gmail.com>
2026-02-21 14:20:44 +0530

    Add methods to check for anagrams using HashMap
    Implement hashMap method to count character frequencies and isAnagram method to check if two strings are anagrams.

--- 242.java
+++ 242.java
@@ -23,3 +23,49 @@ class Solution {
         return mp1.equals(mp2);
     }
 }
+
+
+
+
+
+
+class Solution {
+    public HashMap<Character, Integer> hashMap(String str){
+        HashMap<Character, Integer> mp= new HashMap<>();
+
+        for(int i= 0; i< str.length(); i++){
+            Character ch= str.charAt(i);
+            if(!mp.containsKey(ch)){
+                mp.put(ch, 1);
+            }else{
+                int currKey= mp.get(ch);
+                mp.put(ch, currKey+ 1);
+            }
+        }
+        return mp;
+    }
+
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
+
+        HashMap<Character, Integer> mp= hashMap(s);
+
+        for(int i=0; i< t.length(); i++){
+            Character ch= t.charAt(i);
+            if(mp.containsKey(ch)){
+                int currKey= mp.get(ch);
+                mp.put(ch, currKey- 1);
+                if(mp.get(ch)== 0){
+                    mp.remove(ch);
+                }
+            }else{
+                return false;
+            }
+        }
+
+        for(Integer i: mp.values()){
+            if(i!= 0) return false;
+        }
+        return true;
+    }
+}
