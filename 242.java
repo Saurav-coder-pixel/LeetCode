189a377280970bddd4c27c280e46296c858ba980
Saurav Kumar <sauravkumar7678g@gmail.com>
2026-02-21 14:40:47 +0530

    Implement isAnagram method for string comparison

--- 242.java
+++ 242.java
@@ -69,3 +69,64 @@ class Solution {
         return true;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+       if(s.length() != t.length()) return false;
+
+       int[] arr = new int[26];
+       for(int i=0; i<s.length(); i++){
+        arr[s.charAt(i)- 'a']++;
+        arr[t.charAt(i)- 'a']--;
+       } 
+
+       for(int i: arr){
+        if(i!=0) return false;
+       }
+
+       return true;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+       if(s.length() != t.length()) return false;
+
+       int[] arr = new int[26];
+       for(int i=0; i<s.length(); i++){
+        arr[s.charAt(i)- 'a']++;
+       } 
+
+       for(int i=0; i<t.length(); i++){
+        arr[t.charAt(i)- 'a']--;
+        if(arr[t.charAt(i)- 'a']< 0) return false;
+       }
+
+       for(int i: arr){
+        if(i!=0) return false;
+       }
+
+       return true;
+    }
+}
